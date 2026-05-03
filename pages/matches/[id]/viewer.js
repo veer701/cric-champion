321589fb67c1bbc02297ee8485247a3f3f881a21
veer701 <geetayadav67848@gmail.com>
2026-05-03 14:21:32 +0530

    Create viewer.js file for live scorecard page

---
+++ pages/matches/[id]/viewer.js
@@ -0,0 +1,15 @@
+// pages/matches/[id]/viewer.js
+
+import React from 'react';
+
+const Viewer = () => {
+    return (
+        <div>
+            <h1>Live Scorecard</h1>
+            {/* Include logic here to display the live scorecard */}
+            <p>Scorecard will be updated live.</p>
+        </div>
+    );
+};
+
+export default Viewer;
